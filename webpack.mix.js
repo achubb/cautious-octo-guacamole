7c55c349a898ff26e8dd32dbbe1ef10514f979b0
andy_at_absolute <andrew@Andrews-MacBook-Pro.local>
2017-06-10 16:38:30 +0100

    First working run of postcss-assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,15 +2,20 @@ let mix = require('laravel-mix');
 
 mix.stylus('src/stylus/app.styl', 'dist/css')
     .options({
+        processCssUrls: false,
         postCss: [
             // autoprefixer - does what it says on the tin!
             require('autoprefixer'),
             // add content='' to psuedo elements
-            require('postcss-pseudo-elements-content')
+            require('postcss-pseudo-elements-content'),
+            // postcss - assets
+            require('postcss-assets')({
+                loadPaths: ['dist/images/']
+            })
         ]
     })
    .js('src/js/app.js', 'dist/js')
-   .browserSync({
-      proxy: 'scaf.dev',
-      files: ["dist/css/*.css", "dist/js/*.js"]
-   });
+   // .browserSync({
+   //    proxy: 'scaf.dev',
+   //    files: ["dist/css/*.css", "dist/js/*.js"]
+   // });
