63e220608b418225bf07b6d5e5f1b6b74eeeb350
andy_at_absolute <andrew@Andrews-MacBook-Pro.local>
2017-06-10 15:42:57 +0100

    Conversion from Sass to Stylus

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
-mix.sass('src/sass/app.scss', 'dist/css')
+mix.stylus('src/stylus/app.styl', 'dist/css')
    .js('src/js/app.js', 'dist/js')
    .browserSync({
       proxy: 'scaf.dev',
