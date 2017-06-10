5472801407eaa6a63811bfaf79fdec3315a8382c
andy_at_absolute <andrew@Andrews-MacBook-Pro.local>
2017-06-10 15:51:49 +0100

    Added PostCSS in with prefixer example

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,12 @@
 let mix = require('laravel-mix');
 
 mix.stylus('src/stylus/app.styl', 'dist/css')
+    .options({
+        postCss: [
+            // autoprefixer - does what it says on the tin!
+            require('autoprefixer')
+        ]
+    })
    .js('src/js/app.js', 'dist/js')
    .browserSync({
       proxy: 'scaf.dev',
